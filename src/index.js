0f9aefe2fed4e99e0ed15a34bf88b7637c58c9ab
abdessadek <abdessadekbirama1@gmail.com>
2024-11-11 18:31:53 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDom from 'react-dom/client';
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+root.render(
+    <h1>hello from index.js</h1>
+)
