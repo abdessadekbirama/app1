a274c370aef40f61d7d6273d50da91655b7776e8
abdessadek <abdessadekbirama1@gmail.com>
2024-11-12 08:51:13 +0100

    updates

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import ReactDom from 'react-dom/client';
 const root = ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
+    <>
     <h1>hello from index.js</h1>
+    <h3>update success !</h3>
+    </>
 )
