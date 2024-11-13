5ed4bc4bbd4b8a4795c7d0d8d1f19ef68483c693
abdessadek <abdessadekbirama1@gmail.com>
2024-11-13 07:24:18 +0100

    update

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+function Header(){
+    return(
+        <header className="d-flex align-items-center justify-content-between px-3 bg-warning bg-gradient fixed-top">
+            <h1>My Portfolio</h1>
+            <div className="col-7 nav">
+                <a className="p-3">link1</a>
+                <a className="p-3">link1</a>
+                <a className="p-3">link1</a>
+                <a className="p-3">link1</a>
+            </div>
+        </header>
+    )
+}
+export default Header;
